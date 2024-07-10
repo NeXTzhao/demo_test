9cc47f2a9f54dae7d64fed808a6905f1b51c9717
wangdezhao <wangdezhao@megvii.com>
2024-07-10 21:15:11 +0800

    update

--- Optimal_Control_test/Convex_mpc/convex_mpc.cpp
+++ Optimal_Control_test/Convex_mpc/convex_mpc.cpp
@@ -13,9 +13,9 @@ using namespace proxsuite::proxqp;
 // 通用范数计算函数模板
 template <int Order, int StateDim, int ControlDim>
 typename std::enable_if<Order != 0, double>::type computeNorm(
-    const std::vector<Eigen::Matrix<double, ControlDim, 1>> &delta_u) {
+    const std::vector<Eigen::Matrix<double, ControlDim, 1>>& delta_u) {
   double norm = 0.0;
-  for (const auto &control : delta_u) {
+  for (const auto& control : delta_u) {
     norm += std::pow(control.template lpNorm<Order>(), Order);
   }
   return std::pow(norm, 1.0 / Order);
@@ -24,9 +24,9 @@ typename std::enable_if<Order != 0, double>::type computeNorm(
 // 特化无穷范数
 template <int Order, int StateDim, int ControlDim>
 typename std::enable_if<Order == 0, double>::type computeNorm(
-    const std::vector<Eigen::Matrix<double, ControlDim, 1>> &delta_u) {
+    const std::vector<Eigen::Matrix<double, ControlDim, 1>>& delta_u) {
   double max_norm = 0.0;
-  for (const auto &control : delta_u) {
+  for (const auto& control : delta_u) {
     max_norm = std::max(max_norm, control.template lpNorm<Eigen::Infinity>());
   }
   return max_norm;
@@ -41,16 +41,18 @@ class Vehicle {
   enum StateIndex { X = 0, Y = 1, THETA = 2, V = 3 };
   enum ControlIndex { A = 0, DELTA = 1 };
 
-  explicit Vehicle(double wheelbase, double max_acceleration,
-                   double min_acceleration, double max_steering_angle,
-                   double min_steering_angle)
+  explicit Vehicle(double wheelbase, double _max_speed, double _min_speed,
+                   double _max_acceleration, double _min_acceleration,
+                   double _max_steering_angle, double _min_steering_angle)
       : L(wheelbase),
-        max_a(max_acceleration),
-        min_a(min_acceleration),
-        max_delta(max_steering_angle),
-        min_delta(min_steering_angle) {}
+        max_speed(_max_speed),
+        min_speed(_min_speed),
+        max_a(_max_acceleration),
+        min_a(_min_acceleration),
+        max_delta(_max_steering_angle),
+        min_delta(_min_steering_angle) {}
 
-  State dynamics(const State &state, const Control &control) const {
+  State dynamics(const State& state, const Control& control) const {
     State dState;
     const double v = state(V);
     const double theta = state(THETA);
@@ -62,7 +64,7 @@ class Vehicle {
     return dState;
   }
 
-  State updateState(const State &state, Control &control, double dt) const {
+  State updateState(const State& state, Control& control, double dt) const {
     // Apply control limits
     control = applyControlLimits(control);
 
@@ -75,8 +77,8 @@ class Vehicle {
     return newState;
   }
 
-  void linearize(const State &state, const Control &control, double dt,
-                 Eigen::MatrixXd &A, Eigen::MatrixXd &B) const {
+  void linearize(const State& state, const Control& control, double dt,
+                 Eigen::MatrixXd& A, Eigen::MatrixXd& B) const {
     A = Eigen::MatrixXd::Identity(StateDim, StateDim);
     B = Eigen::MatrixXd::Zero(StateDim, ControlDim);
 
@@ -101,7 +103,7 @@ class Vehicle {
     // clang-format on
   }
 
-  Control applyControlLimits(const Control &control) const {
+  Control applyControlLimits(const Control& control) const {
     Control limited_control = control;
     limited_control(A) = std::max(min_a, std::min(max_a, control(A)));
     limited_control(DELTA) =
@@ -112,7 +114,8 @@ class Vehicle {
  private:
   double L;  // 车辆轴距
  public:
-  double max_a, min_a, max_delta, min_delta;  // 控制输入的限制
+  double max_speed, min_speed, max_a, min_a, max_delta,
+      min_delta;  // 控制输入的限制
 };
 
 template <int StateDim, int ControlDim>
@@ -121,7 +124,7 @@ class MPC {
   using State = typename Vehicle<StateDim, ControlDim>::State;
   using Control = typename Vehicle<StateDim, ControlDim>::Control;
 
-  MPC(const Vehicle<StateDim, ControlDim> &vehicle, int horizon, double dt,
+  MPC(const Vehicle<StateDim, ControlDim>& vehicle, int horizon, double dt,
       Eigen::MatrixXd Q, Eigen::MatrixXd R, Eigen::MatrixXd Q_N)
       : vehicle_(vehicle),
         horizon_(horizon),
@@ -130,8 +133,8 @@ class MPC {
         R_(std::move(R)),
         Q_N_(std::move(Q_N)) {}
 
-  std::vector<Control> solve(const State &initial_state,
-                             const std::vector<State> &reference_trajectory) {
+  std::vector<Control> solve(const State& initial_state,
+                             const std::vector<State>& reference_trajectory) {
     // 初始化优化变量
     std::vector<State> state(horizon_, State::Zero());
     std::vector<Control> control(horizon_ - 1, Control::Zero());
@@ -145,26 +148,34 @@ class MPC {
 
     Eigen::MatrixXd H;
     Eigen::VectorXd g;
-    Eigen::MatrixXd A_t(StateDim, StateDim);
-    Eigen::MatrixXd B_t(StateDim, ControlDim);
-    vehicle_.linearize(initial_state, control[0], dt_, A_t, B_t);
-
     Eigen::MatrixXd A;
     Eigen::VectorXd b;
     Eigen::MatrixXd C;
     Eigen::VectorXd l;
     Eigen::VectorXd u;
 
+    // 设置初始目标函数
     SetCostFunction(H, g, reference_trajectory);
+
+    // 设置初始等式约束
+    Eigen::MatrixXd A_t(StateDim, StateDim);
+    Eigen::MatrixXd B_t(StateDim, ControlDim);
+    vehicle_.linearize(initial_state, control[0], dt_, A_t, B_t);
     SetEqualityConstraints(A, b, A_t, B_t, initial_state);
+
+    // 设置初始不等式约束
     SetInequalityConstraints(C, l, u);
 
+    // cost function
     AINFO << "H dimensions: (" << C.rows() << ", " << C.cols() << ")";
-    AINFO << "A dimensions: (" << A.rows() << ", " << A.cols() << ")";
     AINFO << "g dimensions: (" << g.rows() << ", " << g.cols() << ")";
-    AINFO << "b dimensions: (" << b.rows() << ", " << b.cols() << ")";
+    // dynamic function
     AINFO << "A_t dimensions: (" << A_t.rows() << ", " << A_t.cols() << ")";
     AINFO << "B_t dimensions: (" << B_t.rows() << ", " << B_t.cols() << ")";
+    // equality constraints
+    AINFO << "A dimensions: (" << A.rows() << ", " << A.cols() << ")";
+    AINFO << "b dimensions: (" << b.rows() << ", " << b.cols() << ")";
+    // inequality constraints
     AINFO << "C dimensions: (" << C.rows() << ", " << C.cols() << ")";
     AINFO << "l dimensions: (" << l.rows() << ", " << l.cols() << ")";
     AINFO << "u dimensions: (" << u.rows() << ", " << u.cols() << ")";
@@ -173,88 +184,52 @@ class MPC {
     dense::QP<double> qp_solver(num_vars, num_eq_constraints,
                                 num_in_constraints);
     qp_solver.init(H, g, A, b, C, l, u);
-    qp_solver.solve();
 
-    Eigen::VectorXd solution = qp_solver.results.x;
+    for (int k = 0; k < horizon_ - 1; ++k) {
+      // 更新不等式约束
+      UpdateInequalityConstraints(l, u, state[k], control[k]);
 
-    // 初始化提取的控制输入和状态变量列表
-    std::vector<State> opt_x;
-    std::vector<Control> opt_u;
+      // 重新求解优化问题
+      qp_solver.update(H, g, A, b, C, l, u);
+      qp_solver.solve();
 
-    // 提取初始状态
-    opt_x.push_back(initial_state);
+      // 提取当前时间步的控制输入增量 Δu
+      Eigen::VectorXd solution = qp_solver.results.x;
+      Eigen::VectorXd delta_u = solution.segment(0, ControlDim);
 
-    // 遍历 solution，交替提取 u 和 x
-    for (int i = 0; i < horizon_ - 1; ++i) {
-      Eigen::VectorXd u_i =
-          solution.segment(i * (StateDim + ControlDim), ControlDim);
-      Eigen::VectorXd x_i =
-          solution.segment(i * (StateDim + ControlDim) + ControlDim, StateDim);
-      opt_u.push_back(u_i);
-      opt_x.push_back(x_i);
+      // 更新控制输入
+      control[k] += delta_u;
+
+      // 使用系统动力学模型更新状态
+      state[k + 1] = vehicle_.updateState(state[k], control[k], dt_);
+
+      std::cout << "Control input increment delta_u: " << delta_u.transpose()
+                << std::endl;
+      std::cout << "Ref state: " << reference_trajectory[k + 1].transpose() << std::endl;
+      std::cout << "New state: " << state[k + 1].transpose() << std::endl;
+
+      // 更新下一步的线性化和约束
+      vehicle_.linearize(state[k + 1], control[k], dt_, A_t, B_t);
+      SetEqualityConstraints(A, b, A_t, B_t, state[k + 1]);
     }
 
-    // 提取最后一个控制输入和状态变量
-    Eigen::VectorXd u_last = solution.segment(
-        (horizon_ - 1) * (StateDim + ControlDim) - ControlDim, ControlDim);
-    opt_u.push_back(u_last);
-
-    // 将轨迹数据保存到成员变量
-    trajectory_ = opt_x;
+    trajectory_ = state;
 
     AINFO << "MPC solve complete";
     return control;
   }
 
-  const std::vector<State> &getTrajectory() const { return trajectory_; }
+  const std::vector<State>& getTrajectory() const { return trajectory_; }
 
  private:
-  const Vehicle<StateDim, ControlDim> &vehicle_;
+  const Vehicle<StateDim, ControlDim>& vehicle_;
   int horizon_;
   double dt_;
   std::vector<State> trajectory_;
   Eigen::MatrixXd Q_, R_, Q_N_;
 
-  /*  void SetCostFunction(Eigen::MatrixXd &H,
-                         const std::vector<State> &reference_trajectory) {
-      Eigen::MatrixXd Q =
-          Eigen::MatrixXd::Identity(StateDim, StateDim);  // 状态代价矩阵
-      Eigen::MatrixXd R =
-          Eigen::MatrixXd::Identity(ControlDim, ControlDim);
-          //
-    控制输入代价矩阵 Eigen::MatrixXd Q_N = Eigen::MatrixXd::Identity(StateDim,
-    StateDim);  // 终端状态代价矩阵
-
-      // 计算总变量数
-      int num_rows = (StateDim + ControlDim) * (horizon_ - 1);
-      int num_cols = (StateDim + ControlDim) * (horizon_ - 1);
-
-      // 初始化H矩阵
-      H = Eigen::MatrixXd::Zero(num_rows, num_cols);
-      Eigen::MatrixXd Q_R =
-          Eigen::MatrixXd::Zero(StateDim + ControlDim, StateDim + ControlDim);
-      Q_R.block(0, 0, StateDim, StateDim) = Q;
-      Q_R.block(StateDim, StateDim, ControlDim, ControlDim) = R;
-
-      int blockSize = StateDim + ControlDim;
-      for (int i = 1; i < horizon_ - 1; ++i) {
-        if (i == 0) {
-          H.block(0, 0, ControlDim, ControlDim) = R;
-        } else {
-          H.block((i - 1) * blockSize + ControlDim,
-                  (i - 1) * blockSize + ControlDim, blockSize, blockSize) = Q_R;
-        }
-      }
-
-      // 填充最后一行的终端状态代价矩阵
-      H.block(num_rows - StateDim, num_cols - StateDim, StateDim, StateDim) =
-    Q_N;
-
-      ADEBUG << "Cost function matrix H:\n" << H;
-    }
-    */
-  void SetCostFunction(Eigen::MatrixXd &H, Eigen::VectorXd &g,
-                       const std::vector<State> &reference_trajectory) {
+  void SetCostFunction(Eigen::MatrixXd& H, Eigen::VectorXd& g,
+                       const std::vector<State>& reference_trajectory) {
     int num_rows = (StateDim + ControlDim) * (horizon_ - 1);
     int num_cols = (StateDim + ControlDim) * (horizon_ - 1);
 
@@ -265,9 +240,6 @@ class MPC {
         Eigen::MatrixXd::Zero(StateDim + ControlDim, StateDim + ControlDim);
     Q_R.block(0, 0, StateDim, StateDim) = Q_;
     Q_R.block(StateDim, StateDim, ControlDim, ControlDim) = R_;
-    Eigen::MatrixXd Q_ZERO =
-        Eigen::MatrixXd::Zero(StateDim + ControlDim, StateDim + ControlDim);
-    Q_ZERO.block(0, 0, StateDim, StateDim) = Q_;
 
     H.block(0, 0, ControlDim, ControlDim) = R_;
 
@@ -281,20 +253,21 @@ class MPC {
         Q_N_;
 
     for (int i = 1; i < horizon_; ++i) {
-      const State &xref = reference_trajectory[i];
-      g.segment(ControlDim + (i - 1) * blockSize, StateDim) = Q_ * xref;
+      const State& xref = reference_trajectory[i];
+      g.segment(ControlDim + (i - 1) * blockSize, StateDim) = -Q_ * xref;
     }
-    const State &xref = reference_trajectory[reference_trajectory.size() - 1];
-    g.segment(num_rows - StateDim, StateDim) = -Q_N_ * xref;
+    const State& x_n_ref = reference_trajectory.back();
+    std::cout << "x_back: \n" << x_n_ref <<std::endl;
+    g.tail(StateDim) = -Q_N_ * x_n_ref;
 
     ADEBUG << "Quadratic function matrix H:\n" << H;
     ADEBUG << "Linear function matrix g:\n" << g;
   }
 
-  void SetEqualityConstraints(Eigen::MatrixXd &A, Eigen::VectorXd &b,
-                              const Eigen::MatrixXd &A_t,
-                              const Eigen::MatrixXd &B_t,
-                              const Eigen::VectorXd &x0) {
+  void SetEqualityConstraints(Eigen::MatrixXd& A, Eigen::VectorXd& b,
+                              const Eigen::MatrixXd& A_t,
+                              const Eigen::MatrixXd& B_t,
+                              const Eigen::VectorXd& x0) {
     int num_eq_constraints = StateDim * (horizon_ - 1);
     int a_cols = (ControlDim + StateDim) * (horizon_ - 1);
     A = Eigen::MatrixXd::Zero(num_eq_constraints, a_cols);
@@ -323,8 +296,8 @@ class MPC {
     ADEBUG << "Equality constraint vector b:\n" << b;
   }
 
-  void SetInequalityConstraints(Eigen::MatrixXd &C, Eigen::VectorXd &l,
-                                Eigen::VectorXd &u) {
+  void SetInequalityConstraints(Eigen::MatrixXd& C, Eigen::VectorXd& l,
+                                Eigen::VectorXd& u) {
     int num_in_constraints = (StateDim + ControlDim) * (horizon_ - 1);
 
     C = Eigen::MatrixXd::Identity(num_in_constraints, num_in_constraints);
@@ -335,8 +308,8 @@ class MPC {
     double max_a = vehicle_.max_a;
     double min_delta = vehicle_.min_delta;
     double max_delta = vehicle_.max_delta;
-    double min_v = 0.0;
-    double max_v = 120 / 3.6;
+    double min_v = vehicle_.min_speed;
+    double max_v = vehicle_.max_speed;
     double inf = std::numeric_limits<double>::infinity();
 
     // State bounds
@@ -381,6 +354,49 @@ class MPC {
     ADEBUG << "Inequality lower bound vector l:\n" << l;
     ADEBUG << "Inequality upper bound vector u:\n" << u;
   }
+
+  void UpdateInequalityConstraints(Eigen::VectorXd& l, Eigen::VectorXd& u,
+                                   const State& state, const Control& control) {
+    double min_a = vehicle_.min_a;
+    double max_a = vehicle_.max_a;
+    double min_delta = vehicle_.min_delta;
+    double max_delta = vehicle_.max_delta;
+    double min_v = vehicle_.min_speed;
+    double max_v = vehicle_.max_speed;
+    double inf = std::numeric_limits<double>::infinity();
+
+    Eigen::VectorXd MIN_X(StateDim);
+    MIN_X << -inf, -inf, -inf, min_v;
+
+    Eigen::VectorXd MAX_X(StateDim);
+    MAX_X << inf, inf, inf, max_v;
+
+    Eigen::VectorXd MIN_U(ControlDim);
+    MIN_U << min_a, min_delta;
+
+    Eigen::VectorXd MAX_U(ControlDim);
+    MAX_U << max_a, max_delta;
+
+    Eigen::VectorXd l_block(StateDim + ControlDim);
+    l_block.head(StateDim) = MIN_X;
+    l_block.tail(ControlDim) = MIN_U;
+
+    Eigen::VectorXd u_block(StateDim + ControlDim);
+    u_block.head(StateDim) = MAX_X;
+    u_block.tail(ControlDim) = MAX_U;
+
+    l.segment(0, ControlDim) = MIN_U;
+    u.segment(0, ControlDim) = MAX_U;
+
+    l.segment(l.size() - StateDim, StateDim) = MIN_X;
+    u.segment(u.size() - StateDim, StateDim) = MAX_X;
+
+    for (int t = 1; t < horizon_ - 1; ++t) {
+      int start_idx = (t - 1) * (StateDim + ControlDim) + ControlDim;
+      l.segment(start_idx, StateDim + ControlDim) = l_block;
+      u.segment(start_idx, StateDim + ControlDim) = u_block;
+    }
+  }
 };
 
 int main() {
@@ -388,59 +404,66 @@ int main() {
   const int ControlDim = 2;
 
   double wheelbase = 2.5;
-  double max_acceleration = 3.0;
-  double min_acceleration = -3.0;
-  double max_steering_angle = 0.5;
-  double min_steering_angle = -0.5;
-
-  Eigen::MatrixXd Q =
-      Eigen::MatrixXd::Identity(StateDim, StateDim);  // 状态代价矩阵
-  Eigen::MatrixXd R =
-      Eigen::MatrixXd::Identity(ControlDim, ControlDim);  // 控制输入代价矩阵
-  Eigen::MatrixXd Q_N =
-      Eigen::MatrixXd::Identity(StateDim, StateDim);  // 终端状态代价矩阵
-
-  Vehicle<StateDim, ControlDim> vehicle(wheelbase, max_acceleration,
-                                        min_acceleration, max_steering_angle,
-                                        min_steering_angle);
+  double max_speed = 120 / 3.6;
+  double min_speed = 0.0;
+  double max_acceleration = 10.0;
+  double min_acceleration = -10.0;
+  double max_steering_angle = 2;
+  double min_steering_angle = -2;
 
   int horizon = 10;
-  double dt = 0.1;
+  double dt = 0.2;
+
+  Eigen::MatrixXd Q = Eigen::MatrixXd::Identity(StateDim, StateDim);
+  Q.diagonal() << 50.0, 50.0, 1.0, 0.5;
+  Eigen::MatrixXd R = Eigen::MatrixXd::Identity(ControlDim, ControlDim);
+  R.diagonal() << 1.0, 1.0;
+  Eigen::MatrixXd Q_N = Eigen::MatrixXd::Identity(StateDim, StateDim);
+  Q_N.diagonal() << 10, 10, 10, 0.1;
+
+  Vehicle<StateDim, ControlDim> vehicle(wheelbase, max_speed, min_speed,
+                                        max_acceleration, min_acceleration,
+                                        max_steering_angle, min_steering_angle);
+
+
   MPC<StateDim, ControlDim> mpc(vehicle, horizon, dt, Q, R, Q_N);
 
   Vehicle<StateDim, ControlDim>::State initial_state;
-  initial_state << 0, 0, 0, 0;
+  initial_state << 0.0, 0.0, 0.4, 5;
 
-  std::vector<Vehicle<StateDim, ControlDim>::State> reference_trajectory(
-      horizon + 1, initial_state);
+  std::vector<Vehicle<StateDim, ControlDim>::State> targetTrajectory(
+      horizon, initial_state);
   // 设置参考轨迹为简单的直线
-  for (int i = 0; i <= horizon; ++i) {
-    reference_trajectory[i](Vehicle<StateDim, ControlDim>::X) = i * dt;
-    reference_trajectory[i](Vehicle<StateDim, ControlDim>::Y) = i * dt * 0.5;
+  for (int i = 0; i < horizon; ++i) {
+    targetTrajectory[i](Vehicle<StateDim, ControlDim>::X) = i;
+    targetTrajectory[i](Vehicle<StateDim, ControlDim>::Y) = i;
+    targetTrajectory[i](Vehicle<StateDim, ControlDim>::THETA) = 0.79;
+    targetTrajectory[i](Vehicle<StateDim, ControlDim>::V) = 5;
   }
 
-  auto control_inputs = mpc.solve(initial_state, reference_trajectory);
+  auto control_inputs = mpc.solve(initial_state, targetTrajectory);
 
   // 提取轨迹
-  const auto &trajectory = mpc.getTrajectory();
+  const auto& trajectory = mpc.getTrajectory();
 
   // 提取轨迹中的X和Y坐标
-  std::vector<double> x_ref, y_ref;
-  std::vector<double> x_coords, y_coords;
-  for (const auto &state : reference_trajectory) {
+  std::vector<double> x_ref, y_ref, x_coords, y_coords;
+  for (const auto& state : targetTrajectory) {
     x_ref.push_back(state(Vehicle<StateDim, ControlDim>::X));
     y_ref.push_back(state(Vehicle<StateDim, ControlDim>::Y));
   }
-  for (const auto &state : trajectory) {
+  for (const auto& state : trajectory) {
     x_coords.push_back(state(Vehicle<StateDim, ControlDim>::X));
     y_coords.push_back(state(Vehicle<StateDim, ControlDim>::Y));
   }
 
   // 可视化轨迹
-  plt::named_plot("opt", x_coords, y_coords, "ro-");
   plt::named_plot("ref", x_ref, y_ref, "bo-");
+  plt::named_plot("opt", x_coords, y_coords, "ro-");
   plt::xlabel("X");
   plt::ylabel("Y");
+  plt::grid(true);
+  plt::legend();
   plt::title("Vehicle Trajectory");
   plt::show();
 
